6c7fb081f3673b6c0bc8b7be48b82520b9158079
Amit <amitsunar54321@gmail.com>
2021-07-31 00:15:31 +0530

    3rd Commit

---
+++ src/Algo/Sorting/JustSort.java
@@ -0,0 +1,17 @@
+package Algo.Sorting;
+import java.util.*;
+
+public class JustSort {
+    public static void main(String[] args){
+        int[] arr = {3,2,6,5,1,7,8,9,4,0};
+
+        //This sorting function is from import from the ulit package and this function is using best sorting technique for sort the array
+        Arrays.sort(arr);
+
+        for (int i : arr){
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
