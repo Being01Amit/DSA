b90cb7e9029b5e893672bbebe52e17b639385c6b
Amit <amitsunar54321@gmail.com>
2021-07-31 00:09:13 +0530

    2nd Commit

--- src/Algo/Sorting/Bubble_Sort.java
+++ src/Algo/Sorting/Bubble_Sort.java
@@ -1,33 +1,33 @@
 package Algo.Sorting;
 
 import java.util.*;
-//
-//public class Bubble_Sort {
-//    public static void main(String[] args){
-//
-//        int arr[] = {3,2,6,5,1,7,8,9,4,0};
-//
-//        int n = arr.length;
-//
-////        Arrays.sort(arr);
-////        for(int i: arr){
-////            System.out.println(arr[i]);
-////        }
-//
-//        for (int i = 0; i < n -1; i++){
-//            for(int j = 0; j< n- i -1; j++) {
-//                if (arr[j + 1] < arr[j]) {
-//                    int temp = arr[j];
-//                    arr[j] = arr[j+1];
-//                    arr[j+1] = temp;
-//                }
-//            }
-//        }
-//        for(int i =0 ; i < n ; i++){
-//            System.out.print(arr[i]+" ");
-//        }
-//    }
-//}
+
+public class Bubble_Sort {
+    public static void main(String[] args){
+
+        int[] arr = {3,2,6,5,1,7,8,9,4,0};
+
+        int n = arr.length;
+
+        Arrays.sort(arr);
+        for(int i: arr){
+            System.out.println(arr[i]);
+        }
+
+        for (int i = 0; i < n -1; i++){
+            for(int j = 0; j< n- i -1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
 
 
 
