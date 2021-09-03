45585b58a7e18b359e5853fb343162a6510d948c
Amit <amitsunar54321@gmail.com>
2021-09-04 00:26:13 +0530

    Commit Hashing 1.1 "Union of two Arrays"

--- src/DS/Hashing/Hashset.java
+++ src/DS/Hashing/Hashset.java
@@ -4,18 +4,36 @@ import java.util.HashSet;
 
 
 public class Hashset {
-    public static void main(String[] srgs){
-        int[] arr = {3,2,6,5,1,7,8,9,4,0};
-       int result =  CountDistinct(arr);
+    public static void main(String[] args){
+        int[] a = {3,2,6,5,1};
+        int[] b = {7,8,9,4,0};
+//       int result =  CountDistinct(arr);
+        int result = Union(a,b);
         System.out.println(result);
 
     }
 /* Count Distinct Problem of Hashing Solving using HashSet  */
-    public static int CountDistinct (int[] arr){
+//    public static int CountDistinct (int[] a){
+//        Set<Integer> set = new HashSet<>();
+//        for (int element : arr){
+//            set.add(element);
+//        }
+//        return set.size();
+//    }
+
+    /* Find the Union of the Two Arrays Using the HashSet */
+
+    public static int Union (int[] a , int[] b){
         Set<Integer> set = new HashSet<>();
-        for (int element : arr){
-            set.add(element);
+
+        for (int x : a){
+            set.add(x);
         }
+
+        for (int x : b){
+            set.add(x);
+        }
+
         return set.size();
     }
 }
