1b1d732403bfab8f7035331026e48eee8dd9c360
Amit <amitsunar54321@gmail.com>
2021-09-07 10:50:33 +0530

    Commit Arrays  1.2 "Arrays Right rotates by 1"

--- src/DS/Arrays/OperationOnArrays.java
+++ src/DS/Arrays/OperationOnArrays.java
@@ -2,10 +2,11 @@ package DS.Arrays;
 
 public class OperationOnArrays {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        int[] arr = {1, 2, 3};
         int n = arr.length;
 //        int result = findElement(arr,5);
 //        System.out.println(result);
+//        RightRotateArray(arr,n);
         LeftRotateArray(arr,n);
     }
 
@@ -20,13 +21,26 @@ public class OperationOnArrays {
         return  -1;
     }
 
+    public static void RightRotateArray(int[] arr, int n){
+        int temp = arr[n-1];
+        for (int i = n-1; i> 0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+
+        for (int i =0;i < n ;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
     public static void LeftRotateArray(int[] arr, int n){
         int temp = arr[0];
-        for (int i = 1; i < n; i++){
+        for(int i = 1; i < n; i++){
             arr[i-1] = arr[i];
         }
         arr[n-1] = temp;
-        for (int i =0;i < n ;i++){
+
+        for (int i =0;i < n;i++){
             System.out.print(arr[i]+" ");
         }
     }
